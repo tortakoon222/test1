f9f1e52674dafb1b291476ef3865dd4c120c556d
tortakoon222 <145433724+tortakoon222@users.noreply.github.com>
2023-10-10 14:55:10 +0700

    .

--- Home.py
+++ Home.py
@@ -2,8 +2,8 @@ import json
 import time
 import requests
 import streamlit as st
-from streamlit_lottie import st_lottie
-from streamlit_lottie import st_lottie_spinner
+#from streamlit_lottie import st_lottie
+#from streamlit_lottie import st_lottie_spinner
 
 def load_lottieurl(url: str):
     r = requests.get(url)
@@ -17,9 +17,10 @@ st.subheader("Kairung Hengpraprohm")
 
 col1, col2 = st.columns(2)
 with col1:
-    lot3="https://lottie.host/defef63f-5b59-455d-a6e0-ff03f2791104/t43d8wbLKq.json"
-    lottie3 = load_lottieurl(lot3)
-    st_lottie(lottie3)
+    st.image('./pic/gg.jpg')
+    #lot3="https://lottie.host/defef63f-5b59-455d-a6e0-ff03f2791104/t43d8wbLKq.json"
+    #lottie3 = load_lottieurl(lot3)
+    #st_lottie(lottie3)
 with col2:
     st.image('./pic/DS1.jpg')
 st.balloons()
